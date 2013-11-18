7afe0c4c9971b16710a862a8f085e5f99e9020a5
Rich Wareham <rjw57@cam.ac.uk>
2013-11-18 14:24:29 +0000

    improve visual appearance

--- src/eight-ball-face.c
+++ src/eight-ball-face.c
@@ -3,32 +3,56 @@
 #include "eight-ball-layer.h"
 
 static Window *window;
-static EightBallLayer *eight_ball_layer;
+static TextLayer *text_layer;
 static BitmapLayer *background_layer;
 static GBitmap* background_bitmap;
 
 #define TEXT_LEN 32
 
+static void set_fortune_text(const char* text)
+{
+    Layer *window_layer = window_get_root_layer(window);
+    GRect bounds = layer_get_bounds(window_layer);
+
+    layer_set_frame(text_layer_get_layer(text_layer), (GRect) {
+            .origin = { 20, 20 },
+            .size = { bounds.size.w-40, bounds.size.h-40 }
+    });
+
+    text_layer_set_text(text_layer, text);
+
+    GSize content_size = text_layer_get_content_size(text_layer);
+    layer_set_frame(text_layer_get_layer(text_layer), (GRect) {
+            .origin = { 20, ((bounds.size.h-content_size.h)>>1) - 5},
+            .size = { bounds.size.w-40, content_size.h+20 }
+    });
+}
+
 // Called once per minute
 static void handle_minute_tick(struct tm* tick_time, TimeUnits units_changed) {
     static char tmp_str[TEXT_LEN];
     strftime(tmp_str, sizeof(tmp_str), "%H:%M", tick_time);
-    eight_ball_layer_set_text(eight_ball_layer, tmp_str);
+    set_fortune_text(tmp_str);
 }
 
 static void handle_tap(AccelAxisType axis, int32_t direction)
 {
-    eight_ball_layer_set_text(eight_ball_layer, eight_ball_messages[rand() % EIGHT_BALL_MESSAGE_COUNT]);
+    set_fortune_text(eight_ball_messages[rand() % EIGHT_BALL_MESSAGE_COUNT]);
 }
 
 static void window_load(Window *window) {
     Layer *window_layer = window_get_root_layer(window);
     GRect bounds = layer_get_bounds(window_layer);
 
-    eight_ball_layer = eight_ball_layer_create((GRect) {
-            .origin = { 0, (bounds.size.h-bounds.size.w)>>1},
-            .size = { bounds.size.w, bounds.size.w }
+    text_layer = text_layer_create((GRect) {
+            .origin = { 20, 20 },
+            .size = { bounds.size.w-40, bounds.size.h-40 }
     });
+    text_layer_set_background_color(text_layer, GColorClear);
+    text_layer_set_text_color(text_layer, GColorWhite);
+    text_layer_set_text_alignment(text_layer, GTextAlignmentCenter);
+    text_layer_set_font(text_layer, fonts_get_system_font(FONT_KEY_GOTHIC_28_BOLD));
+    text_layer_set_overflow_mode(text_layer, GTextOverflowModeWordWrap);
 
     background_bitmap = gbitmap_create_with_resource(RESOURCE_ID_BACKGROUND);
 
@@ -45,11 +69,11 @@ static void window_load(Window *window) {
     accel_tap_service_subscribe(&handle_tap);
 
     layer_add_child(window_layer, bitmap_layer_get_layer(background_layer));
-    layer_add_child(window_layer, eight_ball_layer_get_layer(eight_ball_layer));
+    layer_add_child(window_layer, text_layer_get_layer(text_layer));
 }
 
 static void window_unload(Window *window) {
-    eight_ball_layer_destroy(eight_ball_layer);
+    text_layer_destroy(text_layer);
     bitmap_layer_destroy(background_layer);
     gbitmap_destroy(background_bitmap);
 }
